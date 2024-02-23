0d4f7981d03cfee6bb36aef8408bd3e4def9ddd9
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2024-02-23 11:08:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+from tracker import*
+import cvzone
+
+model=YOLO('yolov8s.pt')
+
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap=cv2.VideoCapture(0)
+
+
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n") 
+#print(class_list)
+
+count=0
+
+tracker=Tracker()
+
+cy1=322
+cy2=368
+offset=6
+
+while True:    
+    ret,frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame=cv2.resize(frame,(1020,500))
+   
+
+    results=model.predict(frame)
+ #   print(results)
+    a=results[0].boxes.data
+    px=pd.DataFrame(a).astype("float")
+#    print(px)
+    list=[]
+             
+    for index,row in px.iterrows():
+#        print(row)
+ 
+        x1=int(row[0])
+        y1=int(row[1])
+        x2=int(row[2])
+        y2=int(row[3])
+        d=int(row[5])
+        c=class_list[d]
+        if 'person' in c:
+            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
+            
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(0)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
